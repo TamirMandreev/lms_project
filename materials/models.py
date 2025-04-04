cf2bf68adba94a224f7e57968a177f388f3358e6
Tamir Mandreev <tamirmandreev@mail.ru>
2025-04-04 16:44:33 +0800

    Создал модели Course и Lesson

--- materials/models.py
+++ materials/models.py
@@ -1,3 +1,22 @@
 from django.db import models
 
 # Create your models here.
+
+class Course(models.Model):
+    name = models.CharField(max_length=255, verbose_name="Название курса", help_text='Укажите название курса')
+    preview = models.ImageField(upload_to='materials/preview', blank=True, null=True, verbose_name='Превью курса', help_text='Загрузите превью')
+    description = models.TextField(blank=True, null=True, verbose_name='Описание курса', help_text='Укажите описание курса')
+
+    class Meta:
+        verbose_name = 'Курс'
+        verbose_name_plural = 'Курсы'
+
+
+class Lesson(models.Model):
+    name = models.CharField(max_length=255, verbose_name="Название урока", help_text='Укажите название урока')
+    description = models.TextField(blank=True, null=True, verbose_name='Описание урока',
+                                   help_text='Укажите описание урока')
+    preview = models.ImageField(upload_to='materials/preview', blank=True, null=True, verbose_name='Превью курса',
+                                help_text='Загрузите превью')
+    link_to_video = models.CharField(max_length=255, blank=True, null=True, verbose_name='Ссылка на видео', help_text='Укажите ссылку на видео')
+    course = models.ForeignKey(Course, on_delete=models.SET_NULL, blank=True, null=True, related_name='lessons',)
