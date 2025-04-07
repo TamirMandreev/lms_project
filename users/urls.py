776c9cb5bb21d5cb660d971b12dca2bdeea81b4f
Tamir Mandreev <tamirmandreev@mail.ru>
2025-04-08 03:50:38 +0800

    Реализовал CRUD для модели User

--- users/urls.py
+++ users/urls.py
@@ -1,17 +1,20 @@
 from rest_framework.routers import SimpleRouter
 
 from users.apps import UsersConfig
-from users.views import PaymentViewSet
+from users.views import PaymentViewSet, UserViewSet
 
 app_name =  UsersConfig.name
 
 # Создать экземпляр роутера. Роутер отвечает за автоматическое
 # создание маршрутов для ViewSet на основе стандартных действий CRUD
-router = SimpleRouter()
-router.register('payments', PaymentViewSet, basename='payments')
+router_payment = SimpleRouter()
+router_user = SimpleRouter()
 
+router_payment.register('payments', PaymentViewSet, basename='payments')
+router_user.register('users', UserViewSet, basename='users')
 urlpatterns = [
 
 ]
 
-urlpatterns += router.urls
+urlpatterns += router_payment.urls
+urlpatterns += router_user.urls
--- users/views.py
+++ users/views.py
@@ -1,9 +1,14 @@
 from rest_framework import viewsets
 
-from users.models import Payment
-from users.serializers import PaymentSerializer
+from users.models import Payment, User
+from users.serializers import PaymentSerializer, UserSerializer
 
 
 class PaymentViewSet(viewsets.ModelViewSet):
     queryset = Payment.objects.all()
     serializer_class = PaymentSerializer
+
+
+class UserViewSet(viewsets.ModelViewSet):
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
