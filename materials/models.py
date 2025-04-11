b4b5b5031edad2a95c5c06899fa85498a4bc413d
Tamir Mandreev <tamirmandreev@mail.ru>
2025-04-12 04:34:59 +0800

    Реализовал права доступа

--- materials/models.py
+++ materials/models.py
@@ -1,11 +1,16 @@
 from django.db import models
+from django.db.models import SET_NULL
+
 
 # Create your models here.
+
 
 class Course(models.Model):
     name = models.CharField(max_length=255, verbose_name="Название курса", help_text='Укажите название курса')
     preview = models.ImageField(upload_to='materials/preview', blank=True, null=True, verbose_name='Превью курса', help_text='Загрузите превью')
     description = models.TextField(blank=True, null=True, verbose_name='Описание курса', help_text='Укажите описание курса')
+
+    owner = models.ForeignKey('users.User', on_delete=SET_NULL, null=True, blank=True, verbose_name='Владелец', help_text='Укажите владельца курса')
 
     class Meta:
         verbose_name = 'Курс'
@@ -23,6 +28,8 @@ class Lesson(models.Model):
                                 help_text='Загрузите превью')
     link_to_video = models.CharField(max_length=255, blank=True, null=True, verbose_name='Ссылка на видео', help_text='Укажите ссылку на видео')
     course = models.ForeignKey(Course, on_delete=models.SET_NULL, blank=True, null=True, related_name='lessons',)
+    owner = models.ForeignKey('users.User', on_delete=SET_NULL, null=True, blank=True, verbose_name='Владелец',
+                              help_text='Укажите владельца урока')
 
     class Meta:
         verbose_name = 'Урок'
@@ -30,3 +37,4 @@ class Lesson(models.Model):
 
     def __str__(self):
         return self.name
+
--- materials/views.py
+++ materials/views.py
@@ -1,8 +1,10 @@
 from rest_framework import viewsets
 from rest_framework.generics import CreateAPIView, ListAPIView, RetrieveAPIView, UpdateAPIView, DestroyAPIView
+from rest_framework.permissions import IsAuthenticated
 
 from materials.models import Course, Lesson
 from materials.serializers import CourseSerializer, LessonSerializer
+from users.permissions import IsModerator, IsOwner
 
 
 # Create your views here.
@@ -11,26 +13,51 @@ class CourseViewSet(viewsets.ModelViewSet):
     queryset = Course.objects.all()
     serializer_class = CourseSerializer
 
+    def get_permissions(self):
+        if self.action in ['list', 'retrieve', 'update', 'partial_update']:
+            self.permission_classes = [IsAuthenticated, IsModerator | IsOwner]
+        elif self.action in ['create']:
+            self.permission_classes = [IsAuthenticated, ~IsModerator]
+        elif self.action == 'destroy':
+            self.permission_classes = [IsAuthenticated, ~IsModerator | IsOwner]
+
+        return super().get_permissions()
+
+    def perform_create(self, serializer):
+        course = serializer.save()
+        course.owner = self.request.user
+        course.save()
+
 
 # Представления на основе Generics
 class LessonCreateAPIView(CreateAPIView):
     serializer_class = LessonSerializer
+    permission_classes = [~IsModerator, IsAuthenticated]
+
+    def perform_create(self, serializer):
+        course = serializer.save()
+        course.owner = self.request.user
+        course.save()
 
 
 class LessonListAPIView(ListAPIView):
     queryset = Lesson.objects.all()
     serializer_class = LessonSerializer
+    permission_classes = [IsAuthenticated, IsModerator | IsOwner]
 
 
 class LessonRetrieveAPIView(RetrieveAPIView):
     queryset = Lesson.objects.all()
     serializer_class = LessonSerializer
+    permission_classes = [IsAuthenticated, IsModerator | IsOwner]
 
 
 class LessonUpdateAPIView(UpdateAPIView):
     queryset = Lesson.objects.all()
     serializer_class = LessonSerializer
+    permission_classes = [IsAuthenticated, IsModerator | IsOwner]
 
 
 class LessonDestroyAPIView(DestroyAPIView):
     queryset = Lesson.objects.all()
+    permission_classes = [IsAuthenticated, ~IsModerator | IsOwner]
